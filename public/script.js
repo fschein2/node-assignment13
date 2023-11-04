2cc6b5e6b0025f3d73b2bdd6832880bccd0dfac1
fschein <fschein02@gmail.com>
2023-11-04 18:46:58 -0400

    set up node need to add and style json elements

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const showCanes = async() => {
+    const hurricanesJSON = await getCanes();
+    const canesDiv = document.getElementById("miami-div");
+
+    if (hurricanesJSON == "") {
+        console.log("Invalid load of JSON");
+        return;
+    }
+
+    hurricanesJSON.forEach((cane) => {
+
+    });
+}
+
+const getCanes = async() => {
+    try {
+        return (await fetch("http://localhost:3000/api/hurricanes")).json();
+    } catch (error) {
+        console.log("error retrieving json");
+        return "";
+    }
+};
+
+window.onload = () => showCanes();
--- server.js
+++ server.js
@@ -6,10 +6,71 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/JSON_NAME", (req, res) => {
-    const FILE_NAME = [];
+app.get("/api/hurricanes", (req, res) => {
+    const hurricanes = [];
     
-    res.json(FILE_NAME);
+    hurricanes[0] = {
+        year: "1983",
+        record: "11-1",
+        qb: "Bernie Kosar",
+        topScorers: [
+            "Albert Bentley",
+            "Speedy Neal",
+            "Eddie Brown"
+        ],
+        coach: "Howard Schnellenberger",
+        img: "images/1983canes.jpg"
+    };
+    hurricanes[1] = {
+        year: "1987",
+        record: "12-0",
+        qb: "Steve Walsh",
+        topScorers: [
+            "Melvin Bratton",
+            "Cleveland Gary",
+            "Michael Irvin"
+        ],
+        coach: "Jimmy Johnson",
+        img: "images/1987canes.jpg"
+    };
+    hurricanes[2] = {
+        year: "1989",
+        record: "11-1",
+        qb: "Craig Erickson",
+        topScorers: [
+            "Stephen McGuire",
+            "Dale Dawkins",
+            "Wesley Carroll"
+        ],
+        coach: "Dennis Erickson",
+        img: "images/1989canes.jpg"
+    };
+    hurricanes[3] = {
+        year: "1991",
+        record: "12-0",
+        qb: "Gino Torretta",
+        topScorers: [
+            "Stephen McGuire",
+            "Martin Patton",
+            "Lamar Thomas"
+        ],
+        coach: "Dennis Erickson",
+        img: "images/1991canes.jpg"
+    };
+    hurricanes[4] = {
+        year: "2001",
+        record: "12-0",
+        qb: "Ken Dorsey",
+        topScorers: [
+            "Clinton Portis",
+            "Andre Johnson",
+            "Jeremy Shockey"
+        ],
+        coach: "Larry Coker",
+        img: "images/2001canes.webp"
+    };
+
+    res.json(hurricanes);
 });
 
 app.listen(3000, () => {
